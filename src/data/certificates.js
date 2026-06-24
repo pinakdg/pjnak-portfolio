54289c078c736b99498aed1f68a2a761384739ac
Pinak <pinakdasgupta1412@gmail.com>
2026-06-24 21:13:27 +0530

    new file:   public/certs/ibm.png 	new file:   public/certs/ibm.svg 	modified:   src/data/certificates.js

--- src/data/certificates.js
+++ src/data/certificates.js
@@ -8,4 +8,14 @@ export default [
 //     url: "#",
 //     // badge: "public/certs/try-hack-me.png"
 //   }
+
+{
+    title: "IBM Ethical Hacking with Open Source Tools Specialization",
+    issuer: "IBM",
+    date: "2025-11-15",
+    credentialId: "IYXGG8GN19Q9",
+    url: "https://www.credly.com/badges/a9b48baf-98a0-4145-9537-8fbe0d29ef2d/linked_in_profile",
+    badge: "public/certs/ibm.avg"
+}
+
 ]
